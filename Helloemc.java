06eec7e09f8a34d0dbf7ef8fb955b8050e3be449
iam-nandak <n.kiruthiga9366@gmail.com>
2026-04-19 15:07:24 +0530

    added Java file

---
+++ Helloemc.java
@@ -0,0 +1,8 @@
+public class HelloEMC {
+    public static void main(String[] args) {
+        System.out.println("Hi! This is KiruthigaNSK");
+        System.out.println("Hello EMC Students, welcome to the world of CI/CD");
+        System.out.println("Current Time: " + java.time.LocalDateTime.now());
+        System.out.println("Operating System: " + System.getProperty("os.name"));
+    }
+}
